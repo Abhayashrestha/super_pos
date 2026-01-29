ec4f275e2c312970cc66d53e1f7bb93edd11e6e5
Abhaya Shrestha <avi@Abhayas-MacBook-Pro.local>
2026-01-29 22:44:11 +0545

    feat: implement atomic sales transaction with stock validation and psycopg2

--- data/storage.py
+++ data/storage.py
@@ -3,7 +3,7 @@ def get_connection():
     import psycopg2
     creds={"host":"localhost",
            "database":"",
-           "user":"",
+           "user":"postgres",
            "password":""}
 
     try:
@@ -17,7 +17,7 @@ def get_connection():
 
 connect=get_connection()
 
-def add_product(connection,name,price,quantity,category):
+def db_add_product(connection,name,price,quantity,category):
     try:
         with connection.cursor() as cur:
             sql_query='INSERT INTO products (name,price,quantity,category) VALUES (%s,%s,%s,%s)'
@@ -31,6 +31,57 @@ def add_product(connection,name,price,quantity,category):
     finally:
         cur.close()
 
-add_product(connect,"Apple",10,100,"Fruit")
+#db_add_product(connect,"Apple",10,100,"Fruit")
 
 
+def db_view_all_product(connection):
+    try:
+        with connection.cursor() as cur:
+            display=[]
+            query="SELECT* FROM products"
+            cur.execute(query)
+            key= [column[0] for column in cur.description]
+            for row in cur.fetchall():
+                display.append(dict(zip(key,row)))
+            return display
+
+
+
+    except Exception as e:
+        connection.rollback()
+        print(f"{e} error has occurred")
+
+#db_view_all_product(connect)
+
+def sales_processing(connection,product_id,quantity,customer_name):
+    try:
+        with connection.cursor() as cur:
+            check_sql='SELECT product_id,price,quantity from products WHERE product_id=%s'
+            stock_reduce_sql='UPDATE products SET quantity=quantity-%s WHERE product_id=%s'
+            sales_sql='INSERT INTO sales (customer_name) VALUES (%s) RETURNING sale_id'
+            sale_item_sql='INSERT INTO sale_item (product_id,sale_id,quantity,current_price) VALUES (%s,%s,%s,%s) returning*'
+            cur.execute(check_sql,(product_id,))
+            result = cur.fetchone()
+
+            if not result:
+                raise ValueError("Product Not Found")
+            p_id,current_price,current_quantity=result
+
+            if current_quantity>=quantity:
+                cur.execute(stock_reduce_sql,(quantity,product_id))
+                cur.execute(sales_sql,(customer_name,))
+                s_id=cur.fetchone()[0]
+                line_item=p_id,s_id,quantity,current_price
+                cur.execute(sale_item_sql,line_item)
+                connection.commit()
+                return s_id
+
+            else:
+                raise ValueError("Sorry! We do not have that many in stock")
+
+    except Exception as e:
+        print(f"{e} error has occurred")
+        connection.rollback()
+
+c_name="bob"
+sales_processing(connect,3,10,c_name)
