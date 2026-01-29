b6618b70226f4da7eb0acc7f4f8cd52d297fabfe
Abhaya Shrestha <avi@Abhayas-MacBook-Pro.local>
2026-01-29 15:32:19 +0545

    Switched from volatile memory to static memory

--- data/storage.py
+++ data/storage.py
@@ -1,6 +1,7 @@
+
 def get_connection():
     import psycopg2
-    creds={"host":"",
+    creds={"host":"localhost",
            "database":"",
            "user":"",
            "password":""}
@@ -8,7 +9,28 @@ def get_connection():
     try:
         connection=psycopg2.connect(**creds)
         connection.cursor()
+        print("Connection successful")
         return connection
 
     except (Exception,psycopg2.Error) as error:
         print(f"{error} has occurred")
+
+connect=get_connection()
+
+def add_product(connection,name,price,quantity,category):
+    try:
+        with connection.cursor() as cur:
+            sql_query='INSERT INTO products (name,price,quantity,category) VALUES (%s,%s,%s,%s)'
+            insert_values= name,price,quantity,category
+            cur.execute(sql_query,insert_values)
+            connection.commit()
+            print(f"Product:{name} has been added successfully")
+    except Exception as e:
+        connection.rollback()
+        print(f"{e} error has occurred")
+    finally:
+        cur.close()
+
+add_product(connect,"Apple",10,100,"Fruit")
+
+
